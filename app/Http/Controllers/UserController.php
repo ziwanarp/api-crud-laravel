ad8475d9bc5316660c7640a2e7a498f74a58ab9f
Ziwana Rizwan Pramudia <ziwanarizwan@gmail.com>
2022-12-08 18:41:34 +0700

    publish

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all()->groupBy('birth_place');
+        return response()->json([
+            'user' => $users,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'birth_date' => $request->birth_date,
+            'birth_place' => $request->birth_place,
+            'gender' => $request->gender,
+        ]);
+        return response()->json([
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json([
+            'user' => $user,
+        ]);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->name = $request->name;
+        $user->birth_date = $request->birth_date;
+        $user->birth_place = $request->birth_place;
+        $user->gender = $request->gender;
+        $user->save();
+
+        return response()->json([
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json([
+            'message' => 'User deleted !'
+        ], 204);
+    }
+}
