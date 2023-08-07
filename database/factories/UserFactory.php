edd66319598479c4366d6b993e272d9c166839f8
ziwanarp <ziwanarizwan@gmail.com>
2023-08-07 22:15:43 +0700

    updated

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $gender = ['male', 'female'];
+        return [
+            'name' => $this->faker->name(),
+            'birth_date' => $this->faker->date(),
+            'birth_place' =>$this->faker->city(),
+            'gender' => $gender[rand(0,1)],
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
